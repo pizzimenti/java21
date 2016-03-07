586b9c94e061a4f7c6ce70c3775577bf262af648
Brad Pizzimenti and isabelle george <brad@gennakersystems.com>
2016-03-07 11:17:58 -0800

    http redirect (basic)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,54 @@
 import java.util.*;
-
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+import java.lang.Thread;
 
 import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    
-  }
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      return new ModelAndView (model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/index1", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index1.vtl");
+      return new ModelAndView (model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/index2", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index2.vtl");
+      return new ModelAndView (model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/index3", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index3.vtl");
+      return new ModelAndView (model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    get("/index4", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index4.vtl");
+      return new ModelAndView (model, layout);
+    }, new VelocityTemplateEngine());
+
+  } //end of main
+
+  // public static void timer() {
+  //
+  //   long startTime = System.currentTimeMillis(); //fetch starting time
+  //   while(false||(System.currentTimeMillis()-startTime)<5000)
+  //   {
+  //       get("/")
+  //   }
+  // }
+
 }
