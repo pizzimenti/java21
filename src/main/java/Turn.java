80e07b527ed7b83202fcc7b8d98daed86fbf99b9
Anna Becker and Matt Rosanio <anna.r.becker@gmail.com>
2016-03-07 09:45:04 -0800

    Add methods with tests to Turn class.

--- src/main/java/Turn.java
+++ src/main/java/Turn.java
@@ -2,5 +2,69 @@ import java.util.*;
 import org.sql2o.*;
 
 public class Turn {
-  
+  private int id;
+  private String generatedColor;
+
+  public Turn(String generatedColor) {
+    this.generatedColor = generatedColor;
+  }
+
+  public getId() {
+    return id;
+  }
+
+  public getGeneratedColor() {
+    return generatedColor;
+  }
+
+  public static List<Turn> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM turns";
+      return con.createQuery(sql).executeAndFetch(Turn.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherTurn) {
+    if(!(otherTurn instanceof Turn)) {
+      return false;
+    } else {
+      Turn newTurn = (Turn) otherTurn;
+      return newTurn.getId().equals(id) && newTurn.getGeneratedColor().equals(getGeneratedColor);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO turns (generatedColor) VALUES (:generatedColor)";
+      this.id = (int) con.createQuery(sql, true).addParameter("generatedColor", generatedColor).executeUpdate().getKey();
+    }
+  }
+
+  public static Turn find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM turns WHERE id = :id";
+      return con.createQuery(sql).addParameter("id", id).executeAndFetchFirst(Turn.class);    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM turns";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  public void deleteUserGuess() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE turns SET user_guess = null";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  public void update(String user_guess) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE turns SET user_guess = :user_guess WHERE id = :id";
+      con.createQuery(sql).addParameter("id", id).addParameter("user_guess", user_guess).executeUpdate();
+    }
+  }
 }
---
+++ src/test/java/TurnTest.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import org.sql2o.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TurnTest {
+
+  @Rule
+  DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(0, Turn.all().size());
+  }
+
+  @Test
+  public void equals_returnsTrueIfTurnsHaveSameNameAndMusic() {
+    Turn newTurn = new Turn("yellow");
+    Turn newTurn2 = new Turn("yellow");
+    assertTrue(newTurn.equals(newTurn2));
+  }
+
+  @Test
+  public void save_savesTurnToDatabase() {
+    Turn newTurn = new Turn("yellow");
+    newTurn.save();
+    assertTrue(Turn.all().get(0).equals(newTurn));
+  }
+
+  @Test
+  public void save_savesTurnIdToDatabase() {
+    Turn newTurn = new Turn("yellow");
+    newTurn.save();
+    assertEquals(Turn.all().get(0).getId(),newTurn.getId());
+  }
+
+  @Test
+  public void find_returnsTurnBasedOnId() {
+    Turn newTurn = new Turn("yellow");
+    newTurn.save();
+    Turn newTurn2 = new Turn("blue");
+    newTurn2.save();
+    assertTrue(Turn.find(newTurn.getId()).equals(newTurn));
+    assertTrue(Turn.find(newTurn2.getId()).equals(newTurn2));
+  }
+
+  @Test
+  public void delete_removesTurnFromDatabase() {
+    Turn newTurn = new Turn("yellow");
+    newTurn.save();
+    newTurn.delete();
+    assertEquals(Turn.all().size(), 0);
+  }
+
+  @Test
+  public void deleteUserGuess_removesUserGuessesFromDatabase() {
+    Turn newTurn = new Turn("yellow");
+    newTurn.save();
+    newTurn.update("yellow");
+    Turn newTurn2 = new Turn("blue");
+    newTurn2.save();
+    newTurn2.update("blue")
+    newTurn.deleteUserGuess();
+    assertEquals(newTurn.getUserGuess(), null);
+    assertEquals(newTurn2.getUserGuess(), null);
+  }
+}
